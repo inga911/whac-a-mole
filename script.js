c6c42050d7431e51aaaab089162deabbbbef5ddd
inga911 <inga.banaityte221@gmail.com>
2024-05-15 10:16:21 +0300

    updated  js

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ const pointsHtml = document.querySelector('.points')
 const gameBox = document.querySelector('.game')
 
 let points = 0
-pointsHtml.innerHTML = 'Points: ' + points
+let cellPosition;
+
+function updatePointsHtml(points) {
+    pointsHtml.innerHTML = 'Points: ' + points
+}
+updatePointsHtml(points)
+
 
 
 for (let i = 0; i < 16; i++) {
@@ -10,23 +16,18 @@ for (let i = 0; i < 16; i++) {
         <div class="cell" id="${i}"></div>
         `
 }
+
 const cells = gameBox.querySelectorAll('.cell')
 
-let cellPosition;
 function getRandomCell(cells) {
     cells.forEach(cell => cell.classList.remove('color'));
     let randomCell = cells[Math.floor(Math.random() * cells.length)];
     randomCell.classList.add('color');
     cellPosition = randomCell.id
-
 }
 
 function moveColor() {
-    let timer = null;
-    if (timer) {
-        clearInterval(timer);
-    }
-    timer = setInterval(() => {
+    setInterval(() => {
         getRandomCell(cells);
     }, 1000);
 }
@@ -36,7 +37,7 @@ cells.forEach(cell => {
     cell.onclick = () => {
         if (cell.id === cellPosition) {
             points++
-            pointsHtml.innerHTML = 'Points: ' + points
+            updatePointsHtml(points)
         }
     }
 })
